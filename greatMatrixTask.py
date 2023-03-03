c84e1f282bb1209d2f695066af80d5299353bba6
wibbleydock <pzns1337@gmail.com>
2023-03-03 03:28:58 +0300

    [ADD] add file

---
+++ greatMatrixTask.py
@@ -0,0 +1,21 @@
+matrixA = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+matrixB = [[9, 8, 7],
+           [6, 5, 4],
+           [3, 2, 1]]
+
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+
+for m in range(len(matrixA)):
+    for n in range(len(matrixB[0])):
+        for k in range(len(matrixB)):
+            result[m][n] += matrixA[m][k] * matrixB[k][n]
+
+
+for i in result:
+    print(i)
